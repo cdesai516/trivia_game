90b1eb7668b5b944425de5720ec6356ebe792585
Paradox <cdesai516@gmail.com>
2018-06-08 21:28:23 -0400

    trivia game v1

---
+++ app2.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+console.log("hello");
+var selectedAnswerArr=[];
+var startGameBtn = $('#startGame');
+var timerCounter =$('.timer');
+var number = 30;
+var correctAnswers=["BTC","Satoshi Nakamoto","Blockchain","Ripple","TD Bank","Miners","JP Morgan",];
+var choicesList = $('.choices');
+
+console.log(selectedAnswerArr);
+console.log(startGameBtn);
+console.log(timerCounter);
+
+
+//  Variable that will hold our interval ID when we execute
+//  the "run" function
+var intervalId;
+
+
+
+///// FUNCTIONSSSSSSSSSSSSSSS
+function userAnswerInput() {
+  var t = this;
+  var answerChosen = t.value;
+
+if($('.choices').on('click',function() {
+
+ selectedAnswerArr.push(answerChosen);
+ console.log(selectedAnswerArr);
+})
+
+)}
+
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+  console.log(intervalId);
+}
+
+//  The decrement function.
+function decrement() {
+
+  //  Decrease number by one.
+  number--;
+
+  //  Show the number in the #show-number tag.
+  $("#timer").html("<h2>" + number + "</h2>");
+
+
+  //  Once number hits zero...
+  if (number === 0) {
+
+    //  ...run the stop function.
+    stop();
+
+    //  Alert the user that time is up.
+    alert("Time Up!");
+  }
+}
+//  The stop function
+function stop() {
+
+  //  Clears our intervalId
+  //  We just pass the name of the interval
+  //  to the clearInterval function.
+  clearInterval(intervalId);
+}
+
+//  Execute the run function.
+run();
+});
